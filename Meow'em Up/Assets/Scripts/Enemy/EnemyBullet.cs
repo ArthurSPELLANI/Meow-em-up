27ae8ccd1f53fa740f6820027c57fddcf93590e2
Arthur SPELLANI <a.spellani@rubika-edu.com>
2020-03-18 12:33:24 +0100

    Enemy Test + Death Player + Random fix

---
+++ Meow'em Up/Assets/Scripts/Enemy/EnemyBullet.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float bulletSpeed;
+
+    float damage = 1f;
+
+
+    void Awake()
+    {
+
+    }
+
+    void Start()
+    {
+        rb.velocity = new Vector2(-1, 0) * bulletSpeed;
+    }
+
+    void Update()
+    {
+
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponentInParent<PlayerManager>().TakeDamage(damage);
+            Destroy(this.gameObject);
+        }
+
+        if (collision.gameObject.tag == "Boundaries")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Meow'em Up/Assets/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public float playerHP;
+    public float lives;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerHP <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    } 
+
+    public void TakeDamage(float damage)
+    {
+        playerHP -= damage;
+    }
+}
+
