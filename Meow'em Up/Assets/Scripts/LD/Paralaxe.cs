6eb9e2523c45c1d353a49c22fd11b39fde4bd784
Arthur SPELLANI <a.spellani@rubika-edu.com>
2020-03-19 13:35:33 +0100

    Level 1 Spawner + Background

---
+++ Meow'em Up/Assets/Scripts/LD/Paralaxe.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paralaxe : MonoBehaviour
+{
+
+    private float lenght, startPos;
+    public GameObject backGround;
+    public float parallaxEffect;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position.x;
+        lenght = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        backGround.transform.position = new Vector3(backGround.transform.position.x + speed, transform.position.y, transform.position.z);
+
+        float temp = backGround.transform.position.x * (1 - parallaxEffect);
+        float dist = (backGround.transform.position.x * parallaxEffect);
+        transform.position = new Vector3(startPos + dist, transform.position.y, transform.position.z);
+
+        /*if (temp > startPos + lenght)
+            startPos += lenght;
+        else if (temp < startPos - lenght)
+            startPos -= lenght;*/
+    }
+}
