2ff73d2764d7dd00631ac1498d711dc94744edda
Lolzxad <adelou001@hotmail.com>
2020-03-20 16:28:54 +0100

    Integration commit

---
+++ Meow'em Up/Assets/SoundOnClick.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundOnClick : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+
+    public void ClickSound(AudioClip clipToPlay)
+    {
+        audioSource.clip = clipToPlay;
+        audioSource.Play();
+    }
+
+    public void ClickToStop()
+    {
+        audioSource.Stop();
+    }
+}
